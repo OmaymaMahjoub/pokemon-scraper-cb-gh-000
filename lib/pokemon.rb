83fd221f0d952dd201a44e66288236ac5074d22e
Omayma Mahjoub <omayma.mahjoub@ensi-uma.tn>
2020-07-17 14:53:59 +0000

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,19 @@
 class Pokemon
+
+  @@all=Array.new
+  def initialize(id,name,type,db)
+    @name=name
+    @id=id
+    @type=type
+
+  end
+
+  def self.save(id,name,type,connect_db)
+    if @@all.size<=id
+      connect_db.execute("INSERT INTO pokemon(id,name,type) VALUES (?,?,?)",id,name,type)
+    end
+  end
+
+
+
 end
