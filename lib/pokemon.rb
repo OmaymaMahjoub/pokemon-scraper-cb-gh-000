0baa6ef13cd2ce38f63f8ef44ea37c792cf8b990
Omayma Mahjoub <omayma.mahjoub@ensi-uma.tn>
2020-07-17 15:06:30 +0000

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,5 +1,6 @@
 class Pokemon
 
+  attr_accessor :id,:name,:db,:type
   @@all=Array.new
   def initialize(id:,name:,type:,db:)
     @name=name
@@ -8,12 +9,12 @@ class Pokemon
     @db=db
   end
 
-  def self.save(id,name,type,connect_db)
-    if @@all.size<=id
-      connect_db.execute("INSERT INTO pokemon(id,name,type) VALUES (?,?,?)",id,name,type)
-    end
+  def self.save(name,type,connect_db)
+      connect_db.execute("INSERT INTO pokemon(name,type) VALUES (?,?)",name,type)
   end
 
-
-
+  def self.find(id_num, db)
+    pokemon_info = db.execute("SELECT * FROM pokemon WHERE id=?", id_num).flatten
+    Pokemon.new(id: pokemon_info[0], name: pokemon_info[1], type: pokemon_info[2], db: db)
+  end
 end
